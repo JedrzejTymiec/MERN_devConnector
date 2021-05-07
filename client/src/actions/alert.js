5e623025564762d5a6e801865d8519d3756493b5
Jedrzej Tymiec <jedrek1692@gmail.com>
2021-05-07 17:58:49 +0200

    Alerts

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,10 +1,12 @@
 import { SET_ALERT, REMOVE_ALERT } from "./types";
 import { v4 as uuid } from "uuid";
 
-export const setAlert = (msg, alertType) => (dispatch) => {
+export const setAlert = (msg, alertType, timeout = 2000) => (dispatch) => {
   const id = uuid();
   dispatch({
     type: SET_ALERT,
-    data: { msg, alertType, id },
+    payload: { msg, alertType, id },
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
