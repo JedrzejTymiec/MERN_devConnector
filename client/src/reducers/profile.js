afa54c93b639e2353da856b9ee2121232faaccb2
Jedrzej Tymiec <jedrek1692@gmail.com>
2021-05-11 11:57:34 +0200

    profile list

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,8 +1,10 @@
 import {
   PROFILE_FAIL,
   GET_PROFILE,
+  GET_PROFILES,
   CLEAR_PROFILE,
   UPDATE_PROFILE,
+  GET_REPOS,
 } from "../actions/types";
 
 const initalState = {
@@ -13,7 +15,7 @@ const initalState = {
   error: {},
 };
 
-export default function (state = initalState, action) {
+export default function profileReducer(state = initalState, action) {
   const { type, payload } = action;
 
   switch (type) {
@@ -22,6 +24,12 @@ export default function (state = initalState, action) {
       return {
         ...state,
         profile: payload,
+        loading: false,
+      };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
         loading: false,
       };
     case PROFILE_FAIL:
@@ -35,6 +43,12 @@ export default function (state = initalState, action) {
         ...state,
         profile: null,
         repos: [],
+        loading: false,
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false,
       };
     default:
