b794abdacc9859dabd2585e3d18140bc7756a005
Jedrzej Tymiec <jedrek1692@gmail.com>
2021-05-13 12:40:38 +0200

    404 page

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+import { Fragment } from "react";
+
+const NotFound = (props) => {
+  return (
+    <Fragment>
+      <h1 className="x-large text-primary">
+        <i className="fas fa-exclamation-triangle"></i> Page Not Found
+      </h1>
+      <p className="large">Sorry this page does not exist</p>
+    </Fragment>
+  );
+};
+
+NotFound.propTypes = {};
+
+export default NotFound;
